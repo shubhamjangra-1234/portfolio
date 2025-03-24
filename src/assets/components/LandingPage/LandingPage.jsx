e602e2f66968d9931df14c7574ee17e5dc1ed051
shubham jangra <146321989+shubhamjangra-1234@users.noreply.github.com>
2025-03-24 02:44:20 -0500

    updated

--- src/assets/components/LandingPage/LandingPage.jsx
+++ src/assets/components/LandingPage/LandingPage.jsx
@@ -32,37 +32,35 @@ function LandingPage() {
     return () => clearInterval(interval);
   }, [textData]);
   return (
-   
     <div className="relatives bg-zinc-800 flex h-screen  w-full flex-col items-center justify-center overflow-hidden ">
       <Meteors number={30} />
       <div className="w-full h-full  text-black">
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
- 
-</div>
+        <div
+          style={{ width: "100%", height: "600px", position: "relative" }}
+        ></div>
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
           id="fixedContent"
           className="fixed inset-0  m-auto  flex flex-col items-center justify-center bg-opacity-50 pointer-events-none"
           style={{ height: "85vh" }}
         >
           <div className="p-2">
-            <h2 className="text-zinc-400 mb-4 font-mono text-xl ">
+            <h2 className="text-zinc-400 text-center mb-4 font-mono text-xl ">
               Hello! , I am{" "}
             </h2>
 
             <motion.span
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 3 }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 3 }}
               className=" pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-blacks to-gray-700/80 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-zinc-300 dark:to-slate-700/10"
             >
               Shubham Jangra
             </motion.span>
-           
           </div>
           <p className="font-mono text-xl text-zinc-500 text-center mt-2">
             Unlock Your Web Experience
@@ -70,13 +68,14 @@ function LandingPage() {
           <span className="flex justify-between items-center mt-4">
             {/* <p className="font-mono text-lg"> Skilled at</p> */}
             <button className="h-8 ml-3 border border-zinc-200 rounded-lg opacity-50">
-              <p className="px-2 text-zinc-100 font-mono ">{textData[textIndex]}</p>
+              <p className="px-2 text-zinc-100 font-mono ">
+                {textData[textIndex]}
+              </p>
             </button>
           </span>
         </motion.div>
-       
       </div>
-     
+
       <a href="/main">
         <button className="border-dashed border-zinc-300 z-10 font-mono absolute top-0 right-0 m-4 text-zinc-300  py-2 px-4 rounded-xl border  hover:transition duration-300 ease-in-out flex items-center transform hover:scale-95">
           Get started
@@ -95,7 +94,6 @@ function LandingPage() {
         </button>
       </a>
     </div>
-    
   );
 }
 
